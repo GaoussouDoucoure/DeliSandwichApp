2d134c8df564790ad4a70e20f5961fba3a936c56
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-28 16:44:30 -0700

    Added Menu.java for how the menu will be operated

---
+++ src/Menu.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Menu {
+    static ArrayList<Toppings> regularToppings = new ArrayList<>();
+    static ArrayList<Toppings> premiumMeatToppings = new ArrayList<>();
+    static ArrayList<Toppings> premiumCheeseToppings = new ArrayList<>();
+    static ArrayList<Toppings> bltToppings = new ArrayList<>();
+    static ArrayList<Toppings> pcsToppings = new ArrayList<>();
+    static Sandwich bltSandwich = new Sandwich("White Bread", "MEDIUM", bltToppings,true);
+    static Sandwich pcsSandwich = new Sandwich("White Bread", "MEDIUM", pcsToppings,true);
+}
