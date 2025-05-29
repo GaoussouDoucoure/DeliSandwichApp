6521c68ef970cb01f4c7832f427e8d3808c9834b
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-29 14:23:33 -0700

    Added alltoppings ArrayList

--- src/Menu.java
+++ src/Menu.java
@@ -4,6 +4,7 @@ public class Menu {
     static ArrayList<Toppings> regularToppings = new ArrayList<>();
     static ArrayList<Toppings> premiumMeatToppings = new ArrayList<>();
     static ArrayList<Toppings> premiumCheeseToppings = new ArrayList<>();
+    static ArrayList<Toppings> allToppings = new ArrayList<>();
     static ArrayList<Toppings> bltToppings = new ArrayList<>();
     static ArrayList<Toppings> pcsToppings = new ArrayList<>();
     static Sandwich bltSandwich = new Sandwich("White Bread", "MEDIUM", bltToppings,true);
