8040e0bcb5bacd2c7e8831780fe3374e6b50ec91
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-28 20:39:47 -0700

    Created Drink class to deal with drinks' listings and price calculation

---
+++ src/Drink.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+public class Drink extends Item {
+    protected String drinkSize;
+    protected String drinkFlavor;
+    protected boolean hasIce;
+    
+    static ArrayList<Drink> drinkFlavors = new ArrayList<>();
+
+    public Drink(String drinkSize, String drinkFlavor, boolean hasIce) {
+        this.drinkSize = drinkSize;
+        this.drinkFlavor = drinkFlavor;
+        this.hasIce = hasIce;
+    }
+    
+    public Drink(String drinkFlavor){
+        this.drinkFlavor = drinkFlavor;
+    }
+
+    public String getDrinkSize() {
+        return drinkSize;
+    }
+
+    public void setDrinkSize(String drinkSize) {
+        this.drinkSize = drinkSize;
+    }
+
+    public String getDrinkFlavor() {
+        return drinkFlavor;
+    }
+
+    public void setDrinkFlavor(String drinkFlavor) {
+        this.drinkFlavor = drinkFlavor;
+    }
+
+    public boolean isHasIce() {
+        return hasIce;
+    }
+
+    public void setHasIce(boolean hasIce) {
+        this.hasIce = hasIce;
+    }
+
+    public static void toStringDrinkFlavors() {
+        int i = 1;
+        System.out.println("List of Drink Flavors: ");
+        for (Drink flavors : drinkFlavors) {
+            System.out.println(i + ".) " + flavors.getDrinkFlavor());
+            i++;
+        }
+    }
+
+    @Override
+    public double getPrice() {
+        switch (drinkSize) {
+            case "SMALL" -> {
+                return 2.00;
+            }
+            case "MEDIUM" -> {
+                return 2.50;
+            }
+            case "LARGE" -> {
+                return 3.00;
+            }
+            default -> {
+                return 0.0; //default value when user decides not to get a drink
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (hasIce) {
+            return drinkSize + ", " +
+                    drinkFlavor + ", " +
+                    "Ice";
+        } else {
+            return drinkSize + ", " +
+                    drinkFlavor + ", " +
+                    "No Ice";
+        }
+    }
+
+}
