0e6734abd357129265bd035fb7e42e49fa98737f
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-30 05:09:52 -0700

    Opt out the UserInteface object creation code

--- src/com/java/pluralsight/MainApp.java
+++ src/com/java/pluralsight/MainApp.java
@@ -5,7 +5,5 @@ import static com.java.pluralsight.MenuHandler.menuHandler;
 public class MainApp {
     public static void main(String[] args) {
         menuHandler();
-        UserInterface ui = new UserInterface();
-        ui.homeScreen();
     }
 }
