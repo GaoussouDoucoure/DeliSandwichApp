dafdd207f1b3b18c6b27d253f21127088f112101
Gaoussou Doucoure <gaousd4@gmail.com>
2025-05-30 02:31:59 -0700

    Implemented the signature sandwiches

--- src/com/java/plurasight/Menu.java
+++ src/com/java/plurasight/Menu.java
@@ -1,14 +1,42 @@
 package com.java.plurasight;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Menu {
     static ArrayList<Toppings> regularToppings = new ArrayList<>();
     static ArrayList<Toppings> premiumMeatToppings = new ArrayList<>();
     static ArrayList<Toppings> premiumCheeseToppings = new ArrayList<>();
     static ArrayList<Toppings> allToppings = new ArrayList<>();
-    static ArrayList<Toppings> bltToppings = new ArrayList<>();
-    static ArrayList<Toppings> pcsToppings = new ArrayList<>();
-    static Sandwich bltSandwich = new Sandwich("White Bread", "MEDIUM", bltToppings,true);
-    static Sandwich pcsSandwich = new Sandwich("White Bread", "MEDIUM", pcsToppings,true);
+
+    // Signature Sandwich BLT toppings
+    static ArrayList<Toppings> bltToppings = new ArrayList<>(List.of(
+            new Toppings("Bacon", true, false, false, false),
+            new Toppings("Cheddar", false, true, false, false),
+            new Toppings("Lettuce", false, false, false, false),
+            new Toppings("Tomato", false, false, false, false),
+            new Toppings("Ranch", false, false, false, false)
+    ));
+
+    // Signature Sandwich Philly Cheese Steak Toppings
+    static ArrayList<Toppings> pcsToppings = new ArrayList<>(List.of(
+            new Toppings("Steak", true, false, false, false),
+            new Toppings("American Cheese", false, true, false, false),
+            new Toppings("Peppers", false, false, false, false),
+            new Toppings("Mayo", false, false, false, false)
+    ));
+
+    // Signature Sandwich Chicken Teriyaki Toppings
+    static ArrayList<Toppings> chickenTeriyakiToppings = new ArrayList<>(List.of(
+            new Toppings("Chicken Teriyaki", true, false, false, false),
+            new Toppings("Swiss", false, true, false, false),
+            new Toppings("Lettuce", false, false, false, false),
+            new Toppings("Tomato", false, false, false, false),
+            new Toppings("Mayo", false, false, false, false)
+    ));
+
+    // Signature Sandwiches
+    static Sandwich bltSandwich = new Sandwich("White Bread", "MEDIUM", bltToppings, true, "BLT");
+    static Sandwich pcsSandwich = new Sandwich("White Bread", "MEDIUM", pcsToppings, true, "Philly Cheese Steak");
+    static Sandwich ctSandwich = new Sandwich("Wheat", "LARGE", chickenTeriyakiToppings, true, "Chicken Teriyaki");
 }
